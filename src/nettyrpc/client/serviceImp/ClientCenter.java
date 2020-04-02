ee4bbf31379312b9cc628d37555d13f5595e9918
lossp <richard.xxmxx@gmail.com>
2020-04-02 22:09:26 +0800

    delete the start part in clientCenter

--- src/nettyrpc/client/serviceImp/ClientCenter.java
+++ src/nettyrpc/client/serviceImp/ClientCenter.java
@@ -1,15 +1,7 @@
 package nettyrpc.client.serviceImp;
 
-import nettyrpc.client.handler.ClientCenterHandler;
 import nettyrpc.client.proxy.ObjectProxy;
 import nettyrpc.client.service.Client;
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
 import nettyrpc.registry.ServiceDiscovery;
 
 import java.lang.reflect.Proxy;
@@ -33,23 +25,7 @@ public class ClientCenter implements Client {
     }
 
     public void start() throws Exception{
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
-                    .remoteAddress(new InetSocketAddress(host, port))
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel socketChannel) throws Exception{
-                            socketChannel.pipeline().addLast(new ClientCenterHandler());
-                        }
-                    });
-            ChannelFuture future = bootstrap.connect().sync();
-            future.channel().closeFuture().sync();
-        } finally {
-            group.shutdownGracefully().sync();
-        }
+
     }
 
     public static <T> T create(Class<T> interfaceClass) {
