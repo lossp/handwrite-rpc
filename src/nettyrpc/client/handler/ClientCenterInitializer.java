b542b5c6c70aca1fd2482d9646d82b1cddf9920e
lossp <richard.xxmxx@gmail.com>
2020-04-02 23:10:43 +0800

    put decoder, encoder and clientHandler into the pipe process.

--- src/nettyrpc/client/handler/ClientCenterInitializer.java
+++ src/nettyrpc/client/handler/ClientCenterInitializer.java
@@ -3,16 +3,18 @@ package nettyrpc.client.handler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import nettyrpc.connections.RpcDecoder;
 import nettyrpc.connections.RpcEncoder;
 import nettyrpc.connections.RpcRequest;
+import nettyrpc.connections.RpcResponse;
 
 public class ClientCenterInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) {
         ChannelPipeline channelPipeline = socketChannel.pipeline();
         channelPipeline.addLast(new RpcEncoder(RpcRequest.class));
-        channelPipeline.addLast(new RpcDecoder());
-        channelPipeline.addLast(new RpcRequestHandler());
+        channelPipeline.addLast(new RpcDecoder(RpcResponse.class));
+        channelPipeline.addLast(new ClientCenterHandler());
     }
 
 }
