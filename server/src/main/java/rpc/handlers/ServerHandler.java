5bd039649b583db4ddb950bcdee74aeaced24c55
lossp <richard.xxmxx@gmail.com>
2020-05-28 11:14:44 +0800

    --refactor 增加protobuf文件传输格式，定义相应的proto文件，解决plugin中protobuf出现的无法compile问题

--- server/src/main/java/rpc/handlers/ServerHandler.java
+++ server/src/main/java/rpc/handlers/ServerHandler.java
@@ -1,17 +1,25 @@
 package rpc.handlers;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import valueObject.RpcRequestProto;
 
 
-public class ServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
+public class ServerHandler extends SimpleChannelInboundHandler<RpcRequestProto> {
 
     @Override
-    public void channelRead0(ChannelHandlerContext context, ByteBuf byteBuf) {
+    public void channelRead0(ChannelHandlerContext context, RpcRequestProto request) {
 
     }
 
+    @Override
+    public void channelInactive(ChannelHandlerContext context) {
 
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext context, Throwable cause) {
+
+    }
 
 }
